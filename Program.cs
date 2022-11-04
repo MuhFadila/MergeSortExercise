38d16c53fd5893adb237b75fb1ea13c15cbb9262
Muhammad Fadila <muh.fadila18@gmail.com>
2022-11-04 15:46:11 +0700

    create using system

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 namespace MergeSortExercise
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+{ }
